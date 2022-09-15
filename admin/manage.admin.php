ad2bd7f0d240e92ca46c34a8db1915b13d0fc184
gideon ukaegbu <gideonukaegbu@gideons-MacBook-Pro.local>
2022-09-15 11:50:23 +0100

    initial commit

---
+++ admin/manage.admin.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+
+<title>
+    Food Order Website - Home Page
+</title>
+<link rel="stylesheet" href="../food_app/css/admin.css">
+</head>
+<body>
+
+<!-- Menu Section Starts -->
+ <div class="menu text-center">
+ <div class="wrapper">
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="">Admin Manager</a></li>
+    <li><a href="">Category</a></li>
+    <li><a href="">Food</a></li>
+    <li><a href="">Order</a></li>
+
+</ul>
+
+ </div>   
+</div>
+<!-- Menu Section Ends   -->
+<!-- Main Content Section Starts --> 
+<div class="Main-content">
+<div class="wrapper"><h1>Manage Admin</h1>
+
+ </div>
+<!-- Main  Content Section  Ends   -->
+
+
+<!-- Footer Section Starts --> 
+<div class="Footer Content"> <div class="wrapper"><p class="text-center">  2022 All rights reserved, Some Restaurant. Developed By Gideon Gigu</p></div>
+
+</div>
+<!-- Footer   Section  Ends   -->
+
+</body>
+</html>
